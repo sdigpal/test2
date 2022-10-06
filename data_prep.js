2a3d2075f53906ae511bba591fd34cdc7eb2a314
sdigpal <sdigpal@myseneca.ca>
2022-10-06 14:40:11 -0400

    initail

---
+++ data_prep.js
@@ -0,0 +1,37 @@
+const file = require('fs');  
+var prep = [];
+
+
+exports.initialize = () =>{
+    return new Promise ((resolve, reject) =>{
+        file.readFile('./data/departments.json', (err,data)=> {
+            if (err) {
+                reject ('unable to fetch data');
+            }
+            else {
+                prep = JSON.parse(data);
+            }
+        });    
+        resolve();
+    })
+};
+
+exports.getCpa = () => {
+    return new Promise ((resolve, reject) => {
+        var cpa = students.filter(program => program.isCPA == true);
+        if (cpa.length == 0) {
+            reject('unable to fetch data');
+        }
+        resolve(cpa);
+    })
+};
+
+exports.getHighGpa = () => {
+    return new Promise ((resolve, reject) => {
+        var highGPA = students.filter(gpa => gpa.isgreaterthan4 == true);
+        if (highGPA.length == 0) {
+            reject('unable to fetch data');
+        }
+        resolve(highGPA);
+    })
+};
