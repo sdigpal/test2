f9d1883df0521196e41125229552043eaa5b483c
sdigpal <sdigpal@myseneca.ca>
2022-10-06 14:11:34 -0400

    initial

--- server.js
+++ server.js
@@ -1,53 +1,29 @@
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
-var path = require('path');
 var dataservice = require(__dirname + "/data_prep.js");
 onHttpStart = ()=>{
     console.log('Express http server listening on port ' + HTTP_PORT);
 }
 app.use(express.static('public'));
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname + "/views/home.html"));
-});
+app.get("/", function(req, res){
+    let resText = "<h2>Declaration</h2> <br> <br>";
+    resText += "The rest text is diplayed in the paragraph as shown in the screenshot.<br><br>"; 
+    resText += "I acknowledge the College's academic integrity policy - and my own integrity - remain in effect whether my work is done remotely or onsite. Any test or assingnment is in act of trust between me and my instructor, and specially with my classmates... even when no one is watching. I Declare I will not break that trust.<br><br>"; 
+    resText += "Name: Damanjot Singh<br><br>";
+    resText += "Student Number: 148285216<br><br>"; 
+})
 
-app.get('/home', (req, res)=>{
-    res.sendFile(path.join(__dirname + "/views/home.html"));
-});
 
-app.get('/about', (req, res)=>{
-    res.sendFile(path.join(__dirname + "/views/about.html"));
-});
 
-app.get("/employees", (req, res)=>{
-    dataservice.getAllEmployees().then((data) =>{
-        res.json({data});
-    }).catch((err)=>{
-        res.json({message: err});
-    })
-});
 
-app.get("/managers", (req, res)=>{
-    dataservice.getManagers().then((data) =>{
-        res.json({data});
-    }).catch((err) => {
-        res.json({message: err});
-    })
-});
 
-app.get("/departments", (req, res)=>{
-    dataservice.getDepartments().then((data)=>{
-        res.json({data});
-    }).catch((err) => {
-        res.json({message: err});
-    })
-});
+
 
 app.use((req, res)=>{
     res.status(404).end('404 PAGE NOT FOUND');
 });
-
 dataservice.initialize().then(() => {
     app.listen(HTTP_PORT, onHttpStart());
 }).catch (() => {
