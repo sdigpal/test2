20f2846e6489adb7543781dc17dc3eb6020da27a
sdigpal <sdigpal@myseneca.ca>
2022-10-06 14:14:40 -0400

    initial

--- server.js
+++ server.js
@@ -11,15 +11,9 @@ app.get("/", function(req, res){
     let resText = "<h2>Declaration</h2> <br> <br>";
     resText += "The rest text is diplayed in the paragraph as shown in the screenshot.<br><br>"; 
     resText += "I acknowledge the College's academic integrity policy - and my own integrity - remain in effect whether my work is done remotely or onsite. Any test or assingnment is in act of trust between me and my instructor, and specially with my classmates... even when no one is watching. I Declare I will not break that trust.<br><br>"; 
-    resText += "Name: Damanjot Singh<br><br>";
-    resText += "Student Number: 148285216<br><br>"; 
+    resText += "Name: Sandeep Digpal<br><br>";
+    resText += "Student Number: 126454214<br><br>"; 
 })
-
-
-
-
-
-
 
 app.use((req, res)=>{
     res.status(404).end('404 PAGE NOT FOUND');
